5105b75f183c3bb11a6500ab75476ad25c0ca56f
damilolarandolph <damilolarandolph@gmail.com>
2020-05-17 23:16:34 +0000

    added users and account types table migrations

--- database/migrations/2020_05_17_223646_add_account_types_table.php
+++ database/migrations/2020_05_17_223646_add_account_types_table.php
@@ -4,7 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+
+
+
+
+class AddAccountTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +17,9 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('account_types', function (Blueprint $table) {
             $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('type_description', 20);
         });
     }
 
@@ -30,6 +30,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('account_types');
     }
 }
--- database/migrations/2020_05_17_231301_add_user_account_type.php
+++ database/migrations/2020_05_17_231301_add_user_account_type.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
-
-
-
-class CreateUsersTable extends Migration
+class AddUserAccountType extends Migration
 {
     /**
      * Run the migrations.
@@ -17,11 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 255);
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('account_type')
+                ->references('account_types')
+                ->on('id');
         });
     }
 
@@ -32,6 +27,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
