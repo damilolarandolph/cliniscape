4d40e2834863cce0f5712fc94a490e0403d8b494
damilolarandolph <damilolarandolph@gmail.com>
2020-05-18 03:26:24 +0000

    added bootstrap and dependencies

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,8 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.jQuery = window.$ = require('jquery');
+window.Popper = require("@popperjs/core");
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
